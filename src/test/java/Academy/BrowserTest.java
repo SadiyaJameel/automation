78292906a832db03e7e2a27713bb311f9322434e
sadiya <syedasadiyajameel09@gmail.com>
2026-05-09 21:30:29 +0530

    commit made

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -1,31 +1,29 @@
 package Academy;
 
-import static org.junit.Assert.assertEquals;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
 public class BrowserTest {
 
-	
-	@Test
-	public void getData()
-	{
-		System.out.println("Hello Guys");
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://learndevops-b6bzhtchgaa6efb9.centralus-01.azurewebsites.net/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
-		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
-		driver.close();
-	
-		
-		
-	}
+    @Test
+    public void getData() {
+        System.out.println("Hello Guys");
+        
+        // This handles the driver versioning automatically
+        WebDriverManager.chromedriver().setup();
+        
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://learndevops-b6bzhtchgaa6efb9.centralus-01.azurewebsites.net/");
+        
+        String text = driver.findElement(By.cssSelector("h1")).getText();
+        System.out.println(text);
+        
+        Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+        
+        driver.quit(); // Use .quit() to close all windows and end the session safely
+    }
 }
