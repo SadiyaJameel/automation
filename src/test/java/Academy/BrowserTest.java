3229ab0f037b6f5ff07a5068b40c4e3c92536bfd
sadiya <syedasadiyajameel09@gmail.com>
2026-05-09 21:20:40 +0530

    last commit please

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -1,4 +1,4 @@
-	package Academy;
+package Academy;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,8 +17,8 @@ public class BrowserTest {
 	public void getData()
 	{
 		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://learndevops-b6bzhtchgaa6efb9.centralus-01.azurewebsites.net/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
